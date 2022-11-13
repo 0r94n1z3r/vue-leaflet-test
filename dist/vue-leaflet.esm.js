9a87e475bd5f77ce812c8be72b6befe481b4a81f
0r94n1z3r <43828841+0r94n1z3r@users.noreply.github.com>
2022-11-13 17:03:08 +0300

    Add files via upload

--- dist/vue-leaflet.esm.js
+++ dist/vue-leaflet.esm.js
@@ -1438,8 +1438,7 @@ var script$b = {
 
       propsBinder(methods, {}, props);
 
-      console.log(root.value);
-      //if(typeof root.value == 'Node'){
+      if(root.value){
         const observer = new MutationObserver(scheduleHtmlSwap);
         observer.observe(root.value, {
           attributes: true,
@@ -1447,7 +1446,7 @@ var script$b = {
           characterData: true,
           subtree: true,
         });
-      //}
+      }
       
       scheduleCreateIcon();
     });
